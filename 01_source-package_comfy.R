79e7d73f5b7bcc884cf64ba1fb2e1d78a0068bca
Jim Hester <james.f.hester@gmail.com>
2019-01-10 15:12:29 -0500

    Initial commit

---
+++ 01_source-package_comfy.R
@@ -0,0 +1,16 @@
+#' In a temporary library, do the following.
+## Before each installation restart R to avoid errors due to having the
+## previous version installed.
+
+#' Install the ps _source_ package from CRAN
+## Hint: use `install.packages()` `type` argument
+
+#' Load the package from the temporary library and try running `ps()`
+## The argument to `library` is `lib.loc`
+
+#' Install the develpment version of the ps package
+## You can use either `install_dev()` or `install_github()`
+## Be sure to specify the `lib` argument
+
+#' Install version 1.2.0 of the ps package from CRAN
+## Use install_version()
---
+++ 01_source-package_spartan.R
@@ -0,0 +1,11 @@
+#' In a temporary library, do the following.
+## Before each installation restart R to avoid errors due to having the
+## previous version installed.
+
+#' Install the ps _source_ package from CRAN
+
+#' Load the package from the temporary library and try running `ps()`
+
+#' Install the develpment version of the ps package
+
+#' Install version 1.2.0 of the ps package from CRAN
